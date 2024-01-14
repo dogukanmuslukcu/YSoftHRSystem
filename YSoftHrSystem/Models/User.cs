eae9e5fe936ff6b0b98c840eab4be8f20806e9e0
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-01-14 21:23:30 +0300

    Set Default title object created

--- YSoftHrSystem/Models/User.cs
+++ YSoftHrSystem/Models/User.cs
@@ -11,7 +11,7 @@ namespace YSoftHrSystem.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = "Level1";
         public string HireYear { get; set; }
         public string Salary{ get; set; }
     }
